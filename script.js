12769b26cff0d28f189ac3b36de3f244ef17b070
quintinmp <paviloni5@gmail.com>
2022-12-05 16:15:05 -0700

    fixed contact page issue. added night mode theme

--- script.js
+++ script.js
@@ -32,6 +32,13 @@ function PageTransitions() {
             element.classList.add('active');
         }
     })
+
+    //toggle theme
+    const themeBtn = document.querySelector('.theme-btn');
+    themeBtn.addEventListener('click', () => {
+        let element = document.body;
+        element.classList.toggle('light-mode')
+    })
 }
 
 PageTransitions();
